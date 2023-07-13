e16c5259a9dbd9236ad635d2e94514e87ef638f5
Sushama8 <137862611+Sushama8@users.noreply.github.com>
2023-07-13 00:41:23 -0700

    Add files via upload

---
+++ Practicals/SumOfEvenNumbers.java
@@ -0,0 +1,31 @@
+//print sum of even numbers from 1 to 100
+
+package loops;
+
+public class SumOfEvenNumbers
+{
+
+	public static void main(String[] args)
+	{
+		//num initialized to 1
+        int num = 1;
+        
+        //sum intialized to 0
+        int sum = 0;
+        
+        //condition to check num is less than equal to 100
+        while (num <= 100)
+        {
+        	//if condition to check even number
+            if (num % 2 == 0)
+            {
+                sum += num;
+            }
+            //num is incremented by 1
+            num++;
+        }
+        //print sum of even numbers from 1 to 100
+        System.out.println("Sum of even numbers: " + sum);
+    }
+
+}
