cc83275e1b7c64db7acfdc7ec923b48f8049b144
Sushama8 <137862611+Sushama8@users.noreply.github.com>
2023-07-14 01:54:03 -0700

    Add files via upload

---
+++ Practicals/DoWhileLoopPrac.java
@@ -0,0 +1,22 @@
+//Simple do-while loop example
+package loops;
+
+public class DoWhileLoopPrac
+{
+
+	public static void main(String[] args)
+	{
+		//Declaring and initializing i to 1
+		int i=1;
+		do
+		{
+			//print hello world
+			System.out.println("Hello World");
+			i++;
+		}
+		//Test expression
+		while(i<6);
+
+	}
+
+}
